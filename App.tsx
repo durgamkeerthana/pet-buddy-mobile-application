6f5e90ff67501caad7ceedd6cbfc9e7b315ac3c4
Admin <admin@Admins-MacBook-Pro-5.local>
2024-11-21 12:48:54 +0530

    initialise petBuddy mobile application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
-
-
-
-
 
 
 function App(): React.JSX.Element {
@@ -25,22 +15,6 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
 });
 
 export default App;
